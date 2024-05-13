17e379b2c220ecd00d4ca3142f80b9566e5104ba
venderjin <lffpz3331@gmail.com>
2024-05-13 15:43:06 +0900

    init: first commit

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -3,7 +3,7 @@ import MainNavigation from '@/components/layouts/MainNavigation'
 export default function Home() {
     return (
         <>
-            <div className="flex flex-1 bg-fuchsia-200">홈화면이다잉</div>
+            <div className="flex flex-1 bg-fuchsia-200">홈화면</div>
             <MainNavigation />
         </>
     )
