cc8d5b8e3b92a6dc91a488c5ff95474ca4aa5d2e
venderjin <lffpz3331@gmail.com>
2024-05-16 13:51:47 +0900

    feat: install zod and shadcn ui

---
+++ src/app/(auth)/login/page.tsx
@@ -0,0 +1,7 @@
+export default function Login() {
+    return (
+        <>
+            <div className="flex flex-1 bg-blue-100">로그인화면</div>
+        </>
+    )
+}
---
+++ src/app/(auth)/signup/page.tsx
@@ -0,0 +1,7 @@
+export default function Signup() {
+    return (
+        <>
+            <div className="flex flex-1 bg-green-100 font-Pretendard ">회원가입화면</div>
+        </>
+    )
+}
