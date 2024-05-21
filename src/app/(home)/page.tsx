41a3486ebbefe921e4a742055e8a1c7dc5c35b2b
venderjin <lffpz3331@gmail.com>
2024-05-21 10:23:41 +0900

    feat: modify responsive image

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -1,10 +1,12 @@
 import MainNavigation from '@/components/layouts/MainNavigation'
+// import TestComponent from '@/components/TestComponent '
 
 export default function Home() {
     return (
         <>
             <div className="flex flex-1 bg-fuchsia-200">홈화면</div>
             <MainNavigation />
+            {/* <TestComponent /> */}
         </>
     )
 }
--- src/components/pages/signup/SignUpStep2.tsx
+++ src/components/pages/signup/SignUpStep2.tsx
@@ -6,8 +6,8 @@ export default function SignUpStep2() {
     return (
         <div className="w-full " style={{ height: 'calc(100vh - 60px)' }}>
             <div className="w-full h-1/4 flex flex-col justify-end pb-10 px-10 relative overflow-x-clip ">
-                <div className="absolute -top-14 -right-20">
-                    <ButterMonster width={250} height={250} />
+                <div className="w-1/2 h-5/6  absolute -top-10 -right-20 ">
+                    <ButterMonster />
                 </div>
                 <p className=" font-Pretendard text-[30px] font-extrabold">SIGNUP</p>
                 <p className=" font-Pretendard text-[15px] text-[#646464]">나의 취미를 찾기 위한 여행을 시작합니다.</p>
