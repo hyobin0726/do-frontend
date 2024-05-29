8dfd07da2523f97cc041be14e19e8c00487d771c
venderjin <lffpz3331@gmail.com>
2024-05-29 10:32:45 +0900

    feat: google login success

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -1,12 +1,32 @@
+'use client'
 import MainNavigation from '@/components/layouts/MainNavigation'
-// import TestComponent from '@/components/TestComponent '
+import { useSession, signIn, signOut } from 'next-auth/react'
 
 export default function Home() {
+    const { data: session } = useSession()
+    console.log(session)
+    console.log(process.env.BASE_URL)
+    // return (
+    //     <>
+    //         <div className="flex flex-1 bg-fuchsia-200">홈화면</div>
+    //         <MainNavigation />
+    //         {/* <TestComponent /> */}
+    //     </>
+    // )
+    if (session) {
+        return (
+            <>
+                <div className="flex flex-1 bg-fuchsia-200">홈화면</div>
+                <MainNavigation />
+                Signed in as {session.user.email} <br />
+                <button onClick={() => signOut()}>Sign out</button>
+            </>
+        )
+    }
     return (
         <>
-            <div className="flex flex-1 bg-fuchsia-200">홈화면</div>
-            <MainNavigation />
-            {/* <TestComponent /> */}
+            Not signed in <br />
+            <button onClick={() => signIn()}>Sign in</button>
         </>
     )
 }
