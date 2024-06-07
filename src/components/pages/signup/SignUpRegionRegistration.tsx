97c7cb76fd8e286e6052461dd72039bb81dcc0d3
venderjin <lffpz3331@gmail.com>
2024-06-07 21:19:58 +0900

    feat: region register components create

--- src/components/pages/signup/SignUpRegionRegistration.tsx
+++ src/components/pages/signup/SignUpRegionRegistration.tsx
@@ -1,48 +1,67 @@
 'use client'
 
-// import { useSignUpStore } from '@/hooks/useSignUpStore'
-// import { useRegionStore } from '@/hooks/useRegionStore'
+import { useState, useEffect } from 'react'
 
 import RightArrow from '@/components/images/RightArrow'
 import ProgressBar from '@/components/common/ProgressBar'
-// import Input from '@/components/common/Input'
+import RegionAddInput from '../region/RegionAddInput'
+import { KakaoMapRange } from '@/lib/KakaoMapRange'
 
 export default function SignUpRegionRegistration() {
-    // const { name, id, password, confirmPassword, phoneNumber, email, gender, birthDate } = useSignUpStore()
-    // const { region1, regoin2, region3, setRegion1, setRegion2, setRegion3 } = useRegionStore()
+    const [regionName, setRegionName] = useState<string>('')
+    const [regionCode, setRegionCode] = useState<number>(0)
+    const [regionLatitude, setRegionLatitude] = useState<number>(0)
+    const [regionLongitude, setRegionLongitude] = useState<number>(0)
+    const [regionRange, setRegionRange] = useState<number>(KakaoMapRange[0].id)
 
-    // console.log('name:', name)
-    // console.log('id:', id)
-    // console.log('password:', password)
-    // console.log('confirmPassword:', confirmPassword)
-    // console.log('phoneNumber:', phoneNumber)
-    // console.log('email:', email)
-    // console.log('gender:', gender)
-    // console.log('birthDate:', birthDate)
+    const onRegionChange = (
+        regionName: string,
+        regionCode: number,
+        regionLatitude: number,
+        regionLongitude: number,
+    ) => {
+        setRegionName(regionName)
+        setRegionCode(regionCode)
+        setRegionLatitude(regionLatitude)
+        setRegionLongitude(regionLongitude)
+    }
+
+    const onRegionRangeChange = (range: number) => {
+        setRegionRange(range)
+    }
+
+    useEffect(() => {
+        console.log('regionName: ', regionName)
+        console.log('regionCode: ', regionCode)
+        console.log('regionLatitude: ', regionLatitude)
+        console.log('regionLongitude: ', regionLongitude)
+        console.log('regionRange: ', regionRange)
+    }, [regionName, regionCode, regionLatitude, regionLongitude, regionRange])
 
     return (
         <div className="w-full" style={{ height: 'calc(100svh - 60px)' }}>
-            <div className="w-full h-[20%] flex flex-col justify-end pb-5 px-10 bg-blue-200">
+            <div className="w-full h-[25%] flex flex-col justify-end pb-5 px-10 space-y-3">
                 <p className=" font-Pretendard text-[28px] sm:text-[25px] md:text-[30px] font-extrabold">
                     활동하려는 지역을
                     <br />
                     추가해보세요!
                 </p>
                 <p className=" font-Pretendard text-[13px] sm:text-[12px] md:text-[15px] text-[#646464]">
-                    내 활동 지역은 최대 3개까지 등록할 수 있어요!
+                    내 활동 지역은 마이페이지에서 추가 또는 변경할 수 있습니다.
                 </p>
             </div>
-            {/* <div className="w-full h-[55%] bg-green-100 px-10">
-                <Input
-                    id="region1"
-                    name="region1"
-                    type="text"
-                    placeholder="활동지역을 입력해주세요"
-                    // value={}
-                    onChange={(e) => console.log(e)}
-                    onFocus={() => console.log('focus')}
+            <div className="w-full h-[50%] flex flex-col pt-5 px-10">
+                <RegionAddInput
+                    title="활동지역"
+                    required={true}
+                    id="region"
+                    name="region"
+                    value={regionName}
+                    regionRange={regionRange}
+                    onRegionChange={onRegionChange}
+                    onRegionRangeChange={onRegionRangeChange}
                 />
-            </div> */}
+            </div>
             <div className="w-full h-[25%] px-10 flex flex-col justify-around items-center">
                 <form
                     // onSubmit={handleSubmit(onSubmit)}
