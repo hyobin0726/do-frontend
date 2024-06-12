42dad7b3fd85921141fe61cba6914a00baae5ee6
venderjin <lffpz3331@gmail.com>
2024-06-12 10:28:37 +0900

    feat: mypage profile components create

--- src/app/(member)/mypage/page.tsx
+++ src/app/(member)/mypage/page.tsx
@@ -1,7 +1,58 @@
+import Profile from '@/components/pages/mypage/Profile'
+
 export default function MypagePage() {
     return (
         <>
-            <div className="flex flex-1 bg-fuchsia-100">마이페이지</div>
+            <div className="w-full h-auto bg-hobbing-bg-gray px-5 overflow-y-auto space-y-2">
+                <Profile />
+                <div>마이페이지</div>
+                <div>마이페이지</div>
+                <div>마이페이지</div>
+                <div>마이페이지</div>
+                <div>마이페이지</div>
+                <div>마이페이지</div>
+                <div>마이페이지</div>
+                <div>마이페이지</div>
+                <div>마이페이지</div>
+                <div>마이페이지</div>
+                <div>마이페이지</div>
+                <div>마이페이지</div>
+                <div>마이페이지</div>
+                <div>마이페이지</div>
+                <div>마이페이지</div>
+                <div>마이페이지</div>
+                <div>마이페이지</div>
+                <div>마이페이지</div>
+                <div>마이페이지</div>
+                <div>마이페이지</div>
+                <div>마이페이지</div>
+                <div>마이페이지</div>
+                <div>마이페이지</div>
+                <div>마이페이지</div>
+                <div>마이페이지</div>
+                <div>마이페이지</div>
+                <div>마이페이지</div>
+                <div>마이페이지</div>
+                <div>마이페이지</div>
+                <div>마이페이지</div>
+                <div>마이페이지</div>
+                <div>마이페이지</div>
+                <div>마이페이지</div>
+                <div>마이페이지</div>
+                <div>마이페이지</div>
+                <div>마이페이지</div>
+                <div>마이페이지</div>
+                <div>마이페이지</div>
+                <div>마이페이지</div>
+                <div>마이페이지</div>
+                <div>마이페이지</div>
+                <div>마이페이지</div>
+                <div>마이페이지</div>
+                <div>마이페이지</div>
+                <div>마이페이지</div>
+                <div>마이페이지</div>
+                <div>마이페이지</div>
+            </div>
         </>
     )
 }
