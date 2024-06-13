20120b6f18b96cda201d7538421df6083dab1386
hyobin0726 <hyobin0726@naver.com>
2024-06-13 19:03:45 +0900

    fix: s3 imageUrl

--- next.config.mjs
+++ next.config.mjs
@@ -17,6 +17,10 @@ const nextConfig = {
         GOOGLE_CLIENT_SECRET: process.env.GOOGLE_CLIENT_SECRET,
         KAKAO_API_KEY: process.env.KAKAO_API_KEY,
         KAKAO_REST_API_KEY: process.env.KAKAO_REST_API_KEY,
+        AWS_S3_BUCKET_NAME: process.env.AWS_S3_BUCKET_NAME,
+        AWS_S3_REGION: process.env.AWS_S3_REGION,
+        AWS_S3_ACCESS_KEY: process.env.AWS_S3_ACCESS_KEY,
+        AWS_S3_SECRET_KEY: process.env.AWS_S3_SECRET_KEY,
     },
 }
 
