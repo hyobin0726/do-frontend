6349afbc0d9f111adfdf8cd70c1cf539aa581bd4
hyobin0726 <hyobin0726@naver.com>
2024-06-19 16:02:59 +0900

    fix: boardlist fetching

--- src/components/pages/board/Board.tsx
+++ src/components/pages/board/Board.tsx
@@ -1,4 +1,3 @@
-// 'use server'
 // import BoardProfile from './BoardProfile'
 // import BoardLikeAndComment from './BoardLikeAndComment'
 // import BoardImage from './BoardImage'
@@ -22,49 +21,36 @@
 // }
 
 'use client'
-import { useGetClientToken } from '@/actions/useGetClientToken'
 import { useEffect, useState } from 'react'
 import BoardProfile from './BoardProfile'
 import BoardLikeAndComment from './BoardLikeAndComment'
 import BoardImage from './BoardImage'
 import Link from 'next/link'
 import { BoardType } from '@/type/BoardType'
+import { GetBoard } from '@/api/board/board'
 
 export default function Board({ boardId }: { boardId: string }) {
-    const [board, setBoard] = useState<BoardType>({} as BoardType)
-    const auth = useGetClientToken()
+    const [board, setBoard] = useState<BoardType>()
     useEffect(() => {
-        const GetBoard = async () => {
-            try {
-                const data = await fetch(`${process.env.BASE_URL}/board-service/v1/users/crew/board/${boardId}`, {
-                    method: 'GET',
-                    headers: {
-                        'Content-Type': 'application/json',
-                        Uuid: `${auth.token}`,
-                    },
-                })
-                const response = await data.json()
-                if (response.isSuccess === true) {
-                    setBoard(response.data)
-                    // console.log('게시글을 불러왔습니다.', response.data)
-                } else {
-                    console.error('게시글을 불러오는데 실패했습니다.')
-                }
-            } catch (error) {
-                console.error('네트워크 에러', error)
-            }
+        const fetchBoard = async () => {
+            const getBoard: BoardType = await GetBoard(boardId)
+            setBoard(getBoard)
         }
-        GetBoard()
+        fetchBoard()
     }, [boardId])
 
     return (
         <>
-            <Link href={`/board/${board.boardId}`} className=" space-y-2 ">
-                <BoardProfile writerUuid={board.writerUuid} createdAt={board.createdAt} />
-                <p>{board.content}</p>
-                <BoardImage imageUrls={board.imageUrls} />
-            </Link>
-            <BoardLikeAndComment boardId={boardId} />
+            {board && (
+                <>
+                    <Link href={`/board/${board.boardId}`} className=" space-y-2 ">
+                        <BoardProfile writerUuid={board.writerUuid} createdAt={board.createdAt} />
+                        <p>{board.content}</p>
+                        <BoardImage imageUrls={board.imageUrls} />
+                    </Link>
+                    <BoardLikeAndComment boardId={boardId} />
+                </>
+            )}
         </>
     )
 }
