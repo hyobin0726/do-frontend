0aa234090b5553e00331b93457c130e084fef4de
venderjin <lffpz3331@gmail.com>
2024-06-20 14:11:50 +0900

    feat: current base region box at header and change base region posiable

--- src/app/(home)/layout.tsx
+++ src/app/(home)/layout.tsx
@@ -1,12 +1,19 @@
 import RegionSelector from '@/components/common/RegionSelector'
 import MainHeader from '@/components/layouts/MainHeader'
 import MainNavigation from '@/components/layouts/MainNavigation'
+import getBaseRegion from '@/api/crew/getBaseRegion'
+import getRegionList from '@/api/crew/getRegionList'
 
-export default function HomeLayout({ children }: { children: React.ReactNode }) {
+export default async function HomeLayout({ children }: { children: React.ReactNode }) {
+    const baseRegionData = await getBaseRegion()
+    const regionList = await getRegionList()
+
     return (
         <>
             <MainHeader title="홈">
-                <RegionSelector />
+                {regionList && baseRegionData && (
+                    <RegionSelector baseRegionData={baseRegionData} regionList={regionList} />
+                )}
             </MainHeader>
             {children}
             <MainNavigation />
