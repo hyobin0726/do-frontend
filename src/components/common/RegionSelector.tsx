7eba9aa42d08c60dccf6fa999d643924fb473132
venderjin <lffpz3331@gmail.com>
2024-06-20 14:11:50 +0900

    feat: current base region box at header and change base region posiable

--- src/components/common/RegionSelector.tsx
+++ src/components/common/RegionSelector.tsx
@@ -1,19 +1,76 @@
-import Location from '@/components/images/Location'
-import getBaseRegion from '@/api/crew/getBaseRegion'
+'use client'
 
-export default async function RegionSelector() {
-    const baseRegionData = await getBaseRegion()
+import { useState, useEffect } from 'react'
+import { useRouter } from 'next/navigation'
+import Location from '@/components/images/Location'
+import postBaseRegion from '@/api/crew/postBaseRegion'
+
+import {
+    DropdownMenu,
+    DropdownMenuContent,
+    DropdownMenuRadioGroup,
+    DropdownMenuRadioItem,
+    DropdownMenuTrigger,
+} from '@/components/ui/dropdown-menu'
+
+interface RegionType {
+    regionId: number
+    addressName: string
+}
+
+export default function RegionSelector({
+    baseRegionData,
+    regionList,
+}: {
+    baseRegionData: RegionType
+    regionList: RegionType[]
+}) {
+    const [position, setPosition] = useState<RegionType>(baseRegionData)
+    const router = useRouter()
+
+    useEffect(() => {
+        if (position.regionId !== baseRegionData.regionId) {
+            postBaseRegion(position.regionId)
+            router.refresh()
+        }
+    }, [position])
 
     return (
-        <>
-            <div className="w-3/5 h-full pl-3 space-x-3 rounded-xl border-[1px] flex flex-row justify-between items-center bg-hobbing-light-pink border-hobbing-pink">
-                <div className="flex flex-row items-center">
-                    <Location />
+        <DropdownMenu>
+            <DropdownMenuTrigger asChild>
+                <div className="w-[150px] h-full pl-3 space-x-2 rounded-xl border-[1px] flex flex-row justify-between items-center bg-hobbing-light-pink border-hobbing-pink">
+                    <div className="flex flex-row items-center">
+                        <Location />
+                    </div>
+                    <div className="w-full h-full flex flex-col justify-center">
+                        <p className="text-sm font-medium">{baseRegionData.addressName}</p>
+                    </div>
                 </div>
-                <div className="w-full h-full flex flex-col justify-center">
-                    <p className="text-sm font-medium">{baseRegionData.addressName}</p>
-                </div>
-            </div>
-        </>
+            </DropdownMenuTrigger>
+            <DropdownMenuContent className="w-[150px] bg-hobbing-light-pink z-[1000]">
+                <DropdownMenuRadioGroup
+                    value={position}
+                    onValueChange={(value) => {
+                        setPosition(value as RegionType)
+                    }}
+                    className="m-1"
+                >
+                    {regionList.map((region) => (
+                        <DropdownMenuRadioItem
+                            key={region.regionId}
+                            value={region}
+                            className={`${position.addressName == region.addressName ? 'bg-hobbing-bg-pink rounded-md' : ''}`}
+                        >
+                            <div
+                                className={`flex flex-row items-center pr-2 ${position.addressName != region.addressName ? 'opacity-0 ' : ''}`}
+                            >
+                                <Location />
+                            </div>
+                            {region.addressName}
+                        </DropdownMenuRadioItem>
+                    ))}
+                </DropdownMenuRadioGroup>
+            </DropdownMenuContent>
+        </DropdownMenu>
     )
 }
