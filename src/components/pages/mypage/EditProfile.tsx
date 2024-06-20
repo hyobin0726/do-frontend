b8373a2b5275efd05a717bffc37977a9f322562b
venderjin <lffpz3331@gmail.com>
2024-06-20 14:56:57 +0900

    feat: edit profile pusblishing

---
+++ src/components/pages/mypage/EditProfile.tsx
@@ -0,0 +1,23 @@
+export default function EditProfile() {
+    return (
+        <>
+            <main>
+                <section>
+                    <div>
+                        <h1>프로필 수정????????</h1>
+                    </div>
+                </section>
+                <section>
+                    <div>
+                        <h1>프로필 수정????????</h1>
+                    </div>
+                </section>
+                <section>
+                    <div>
+                        <h1>프로필 수정????????</h1>
+                    </div>
+                </section>
+            </main>
+        </>
+    )
+}
