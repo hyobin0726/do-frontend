8d71fecb0b218a9ef752abe445f99a08847a6af3
venderjin <lffpz3331@gmail.com>
2024-06-21 18:03:10 +0900

    feat: profile edit fetch

--- src/api/auth/putMyProfile.ts
+++ src/api/auth/putMyProfile.ts
@@ -1,18 +1,25 @@
 'use server'
 
 import { useGetServerToken } from '@/actions/useGetServerToken'
+import { revalidateTag } from 'next/cache'
 
-export default async function getMyProfile() {
+export default async function putMyProfile(profileImageUrl: string, profileMessage: string) {
     const auth = await useGetServerToken()
 
     const res = await fetch(`${process.env.BASE_URL}/auth-service/v1/users/member/profile`, {
-        method: 'GET',
+        method: 'PUT',
         headers: {
             Authorization: `${auth.token}`,
             'Content-Type': 'application/json',
         },
-        next: { tags: ['myProfile'] },
+        body: JSON.stringify({
+            profileImageUrl: profileImageUrl,
+            profileMessage: profileMessage,
+        }),
     })
     const data = await res.json()
-    return data.data
+    if (data.isSuccess) {
+        revalidateTag('myProfile')
+    }
+    return data
 }
