89617d94591ad0f9ca955d0afbd89e6f15c481a9
venderjin <lffpz3331@gmail.com>
2024-06-25 09:48:30 +0900

    refactor: signup and login refactoring

--- src/components/pages/signup/SignUpStep2.tsx
+++ src/components/pages/signup/SignUpStep2.tsx
@@ -5,7 +5,7 @@ export default function SignUpStep2() {
     return (
         <main className="w-[100%] h-dvh" style={{ height: 'calc(100svh - 60px)' }}>
             <section className="w-full h-[15%] flex flex-col justify-end pb-5 px-10 relative overflow-x-clip ">
-                <div className="w-1/2 h-5/6  absolute -top-10 -right-20 ">
+                <div className="w-1/2 h-5/6  absolute -top-10 -right-20 z-[500]">
                     <ButterMonster />
                 </div>
                 <p className=" font-Pretendard text-[28px] sm:text-[25px] md:text-[30px] font-extrabold">SIGNUP</p>
