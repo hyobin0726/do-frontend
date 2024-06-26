6f09400bb777a0be6740d4b442b29d361080b2c7
venderjin <lffpz3331@gmail.com>
2024-06-26 15:48:43 +0900

    test: test commit

--- src/components/pages/home/HomeSection2.tsx
+++ src/components/pages/home/HomeSection2.tsx
@@ -1,6 +1,6 @@
 'use client'
 
-import { useState, useEffect, use } from 'react'
+import { useState, useEffect } from 'react'
 import Link from 'next/link'
 
 import getNewCrew from '@/api/crew/getNewCrew'
