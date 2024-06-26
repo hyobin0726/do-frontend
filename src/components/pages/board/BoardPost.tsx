c53841689ec5998d5b83dc7ea0944b24e82b1b26
hyobin0726 <hyobin0726@naver.com>
2024-06-26 19:31:49 +0900

    refactor: board post severaction

--- src/components/pages/board/BoardPost.tsx
+++ src/components/pages/board/BoardPost.tsx
@@ -1,6 +1,6 @@
 export default function BoardPost() {
     return (
-        <div className="w-14 h-[37px]  bg-hobbing-red border-[1px] border-hobbing-red rounded-xl flex justify-center items-center">
+        <div className="w-20 h-[37px]  bg-hobbing-red border-[1px] border-hobbing-red rounded-xl flex justify-center items-center">
             <p className="font-Pretendard text-white text-[13px] font-bold">발행</p>
         </div>
     )
