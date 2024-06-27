55c8d8f42df2792e2560c199ef3fd037c812e85f
venderjin <lffpz3331@gmail.com>
2024-06-27 10:00:52 +0900

    feat: survey progressbar conponenet add

--- src/app/(survey)/survey/page.tsx
+++ src/app/(survey)/survey/page.tsx
@@ -1,18 +1,23 @@
+import Link from 'next/link'
+
 import getSurveyQuestions from '@/api/survey/getSurveyQuestions'
+import ProgressBar from '@/components/common/ProgressBar'
 
 export default async function SurveyPage({ searchParams }: { searchParams: { [key: string]: number } }) {
     const surveyQuestions = await getSurveyQuestions()
     const surveyStep = Number(searchParams.step)
 
-    // console.log('surveyQuestions : ', surveyQuestions)
-    console.log('surveyStep : ', surveyStep)
-
     return (
         <>
             <main className="w-full h-[calc(100dvh-60px)] bg-green-100 px-10">
                 <section className="bg-yellow-50 w-full h-[30%]">test</section>
-                <section className="bg-yellow-100 w-full h-[50%]">test</section>
-                <section className="bg-yellow-200 w-full h-[20%]">test</section>
+                <section className="bg-yellow-100 w-full h-[50%]">
+                    <Link href={`/survey?step=${surveyStep + 1}`}>test</Link>
+                </section>
+                <section className="bg-yellow-200 w-full h-[20%]">
+                    test
+                    <ProgressBar step={surveyStep} total={surveyQuestions.data.length} />
+                </section>
             </main>
         </>
     )
