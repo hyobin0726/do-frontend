5877f2fea4495512dc44c4faa60c6325a3cd4f1c
venderjin <lffpz3331@gmail.com>
2024-06-29 21:29:44 +0900

    refactor: middleware modify and signup schema modify

--- src/middleware.tsx
+++ src/middleware.tsx
@@ -1,16 +1,55 @@
-import withAuth from 'next-auth/middleware';
-import { pagesOptions } from './app/api/auth/[...nextauth]/page-options';
+// import withAuth from 'next-auth/middleware';
+// import { pagesOptions } from './app/api/auth/[...nextauth]/page-options';
 
-export default withAuth({
-  pages: {
-    ...pagesOptions,
-  },
-});
+// export default withAuth({
+//   pages: {
+//     ...pagesOptions,
+//   },
+// });
+
+// export const config = {
+//   // restricted routes
+//   matcher: [
+//     '/',
+//     '/:path*',
+//   ],
+// };
+
+export { default } from 'next-auth/middleware'
+
+import type { NextRequest } from 'next/server'
+import { NextResponse } from 'next/server'
+import { getToken } from 'next-auth/jwt'
+
+const secret = process.env.SECRET
+
+export async function middleware(req: NextRequest) {
+    const session = await getToken({ req, secret, raw: true })
+    const { pathname } = req.nextUrl
+
+    // 정적 파일 및 API 요청 무시
+    if (
+        pathname.startsWith('/_next') ||
+        pathname.startsWith('/static') ||
+        pathname.startsWith('/api') ||
+        pathname.includes('.')
+    ) {
+        return NextResponse.next()
+    }
+
+    // 로그인한 사용자 접근 제한
+    if ((pathname.startsWith('/login') || pathname.startsWith('/signup')) && session) {
+        return NextResponse.redirect(new URL('/', req.url))
+    }
+
+    // 로그인하지 않은 사용자 접근 제한
+    if (!session && pathname !== '/login' && pathname !== '/signup' && !pathname.startsWith('/account')) {
+        return NextResponse.redirect(new URL('/login', req.url))
+    }
+
+    return NextResponse.next()
+}
 
 export const config = {
-  // restricted routes
-  matcher: [
-    '/',
-    '/:path*',
-  ],
-};
+    matcher: ['/:path*'],
+}
